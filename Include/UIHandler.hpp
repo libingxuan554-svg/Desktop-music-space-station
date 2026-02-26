7b8ca7ff1499d574b94231781aebb7592f173881
YangZhang-001 <18856301287@163.com>
2026-02-26 18:34:04 +0000

    Update UIHandler.hpp

--- Include/UIHandler.hpp
+++ Include/UIHandler.hpp
@@ -4,18 +4,19 @@
 #include <vector>
 #include <string>
 
-/**
- * @brief Abstract Interface for UI Event Handling
- * Focus: Decoupling UI from Audio/Hardware threads using callbacks.
- */
+/* 
+@brief Abstract Interface for UI Event Handling
+## Focus: Decoupling UI from Audio/Hardware threads using callbacks ##
+*/
+
 class UIHandler {
 public:
     virtual ~UIHandler() = default;
 
-    // --- HARDWARE DRIVEN EVENTS (Inbound) ---
+    // ### HARDWARE DRIVEN EVENTS (Inbound) ###
 
     // Triggered by Infrared Sensor thread to switch UI layouts
-    // isMusicActive: true for Player View, false for Clock/Weather View
+    // isMusicActive: true for Player View, false for Clock/Weather/Temperature View
     virtual void onSystemModeChanged(bool isMusicActive) = 0;
 
     // Triggered by Audio Analysis thread (FFT result)
@@ -23,14 +24,16 @@ public:
     virtual void onVisualizerDataReady(const std::vector<float>& frequencyBins) = 0;
 
     // Triggered by Temperature/Weather thread
+    // currentTemp: The real-time temperature value on the standly screen
+    // weatherCondition: String data used to update the environmental UI icons   ！！To be defined ！！
     virtual void onEnvironmentUpdate(float currentTemp, const std::string& weatherCondition) = 0;
 
-    // --- USER INTERACTION CALLBACKS (Outbound) ---
+    // ### USER INTERACTION CALLBACKS (Outbound) ###
     
     // Callbacks provided to UI to control the Media branch
     struct InteractionSignals {
-        void (*requestPlayPause)();
-        void (*requestVolumeAdjust)(int level);
+        void (*requestPlayPause)(); //Play/Pause Callback
+        void (*requestVolumeAdjust)(int level);  //Volume adjustment slider
     };
 };
 
