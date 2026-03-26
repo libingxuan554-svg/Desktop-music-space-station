3e861cfb2bd50c370e4e110460c71f8b5e2f0c81
ZiKai2002 <3079284M@student.gla.ac.uk>
2026-03-26 21:43:55 +0000

    Update LedStripController.hpp

--- LedStripController.hpp
+++ LedStripController.hpp
@@ -5,14 +5,17 @@
 class LedStripController {
 public:
     bool initialize(const char* device = "/dev/spidev0.0", uint32_t speed = 3200000);
-    void updateFromPeak(float peak);
+    void updateFromSpectrum(const std::vector<float>& spectrum);
     void clear();
     void shutdown();
 
 private:
     void sendLeds(const std::vector<uint32_t>& colors);
+    static uint32_t makeGRB(uint8_t g, uint8_t r, uint8_t b);
 
     int fd = -1;
+    std::vector<float> m_smoothedSpectrum;
+
     static constexpr int LED_COUNT = 60;
     static constexpr int RESET_BYTES = 500;
 };
